a30ce09ad6f74478dd72a53d6738fa9f1b5e16f4
Aqima Adalahita <144989915+aqimaadalahita@users.noreply.github.com>
2024-10-05 19:39:38 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 import plotly.express as px
-from babel.numbers import format_currency
 sns.set(style='dark')
 
 # Load the dataset
